7d8a1871f2b412ba210ee0c8b3ba07cc46c6a7e2
shiqugao <1147965943@qq.com>
2023-04-19 11:44:21 +0100

    movie search

---
+++ Flutter Apps/myfirstapp/lib/SearchMoviePage.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+
+  Movie({required this.id, required this.title, required this.overview});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+    );
+  }
+}
+
+class MovieSearch extends StatefulWidget {
+  @override
+  _MovieSearchState createState() => _MovieSearchState();
+}
+
+class _MovieSearchState extends State<MovieSearch> {
+  final _searchController = TextEditingController();
+  List<Movie> _movies = [];
+  bool _isLoading = false;
+
+  Future<List<Movie>> _searchMovies(String query) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final response = await http.get(
+        Uri.parse('https://api.themoviedb.org/3/search/movie?query=$query&api_key=YOUR_API_KEY'));
+
+    if (response.statusCode == 200) {
+      final movies = jsonDecode(response.body)['results'] as List;
+      setState(() {
+        _movies = movies.map((json) => Movie.fromJson(json)).toList();
+        _isLoading = false;
+      });
+      return _movies;
+    } else {
+      throw Exception('Failed to search movies');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie Search'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search for a movie',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () async {
+                    await _searchMovies(_searchController.text);
+                  },
+                ),
+              ),
+            ),
+          ),
+          if (_isLoading)
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+          if (_movies.isNotEmpty)
+            Expanded(
+              child: ListView.builder(
+                itemCount: _movies.length,
+                itemBuilder: (context, index) {
+                  final movie = _movies[index];
+                  return ListTile(
+                    title: Text(movie.title),
+                    subtitle: Text(movie.overview),
+                  );
+                },
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- Flutter Apps/myfirstapp/lib/main.dart
+++ Flutter Apps/myfirstapp/lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'SearchMoviePage.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
+    return MaterialApp(
+
+        title: 'Book search',
+        theme: new ThemeData(
+          primaryColor: new Color(0xFF0F2533),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
-        ),
-      ),
-    );
+        routes: {
+          '/': (BuildContext context) => new MovieSearch(),
+          //'/search_material': (BuildContext context) => new MovieSearch(),
+          // '/search_formal': (BuildContext context) => new SearchBookPageNew(),
+          // '/collection': (BuildContext context) => new CollectionPage(),
+          // '/stamp_collection_material': (BuildContext context) => new StampCollectionPage(),
+          // '/stamp_collection_formal': (BuildContext context) => new StampCollectionFormalPage(),
+        },
+      );
   }
 }
